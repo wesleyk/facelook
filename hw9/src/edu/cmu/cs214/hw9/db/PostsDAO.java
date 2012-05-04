7337ac7f62ff42ec08dc4072931f5e374f8256ef
Wesley Kim <wesleyjkim@gmail.com>
2012-05-04 01:34:17 -0400

    clean up

--- hw9/src/edu/cmu/cs214/hw9/db/PostsDAO.java
+++ hw9/src/edu/cmu/cs214/hw9/db/PostsDAO.java
@@ -4,7 +4,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 
 import json.JSONArray;
 import json.JSONObject;
